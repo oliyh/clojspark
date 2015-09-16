d6a7c883934d00f13120b8aa8b92d690ab097a61
Oliver Hine <git@oliy.co.uk>
2015-09-16 18:35:12 +0100

    Stuff working locally

--- clojure/src/spark/core.clj
+++ clojure/src/spark/core.clj
@@ -3,27 +3,59 @@
             [sparkling.conf :as conf]
             [sparkling.core :as spark]
             [sparkling.destructuring :as s-de])
-  (:gen-class)
-  )
+  (:gen-class))
 
+(defn ->sale-record [line]
+  (zipmap [:price :date :postcode]
+          (map #(s/replace % #"\"" "") (drop 1 (s/split line #",")))))
 
+(defn do-run [sc]
+  (let [input-rdd (spark/text-file sc "hdfs://localhost/user/clojspark/basics/inputdata/house-prices.csv")
+        postcode-groups (->> (spark/flat-map (fn [l] (s/split l #"\n")) input-rdd)
+                             (spark/map-to-pair (fn [l]
+                                                  (let [record (->sale-record l)]
+                                                    (spark/tuple (first (s/split (:postcode record) #" ")) record))))
+                             (spark/group-by-key))]
 
+    (println (spark/take 10 postcode-groups))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Basic Spark management
 
-(defn make-spark-context []
-  (let [c (-> (conf/spark-conf)
-              ;;(conf/master "local")
-              (conf/app-name "Clojure Spark example"))]
+(defn make-spark-context [local?]
+  (let [c (if local?
+            (-> (conf/spark-conf)
+                (conf/master "local")
+                (conf/app-name "Clojure Spark example"))
+            (-> (conf/spark-conf)
+                (conf/app-name "Clojure Spark example")))]
     (spark/spark-context c)))
 
+(defonce local-sc (memoize (partial make-spark-context true)))
 
+(defn run-local []
+  (let [sc (local-sc)]
+    (do-run sc)))
 
 (defn -main [& args]
-  (let [sc (make-spark-context)
-        input-rdd (spark/text-file sc "hdfs://localhost/user/clojspark/basics/inputdata")
-	  words-rdd (spark/flat-map (fn [l] (s/split l #" ")) input-rdd)
-	  count-rdd (->> (spark/map-to-pair (fn [w] (spark/tuple w 1)) words-rdd) (spark/reduce-by-key +) )
-	  res (spark/map (s-de/key-value-fn (fn [k v] (str "(" k ", " v ")"))) count-rdd) ]
-    (println (spark/collect res))))
+  (do-run (make-spark-context false)))
+
+
+(comment                              )
+
+        (comment lowest-prices (->> outcode-prices
+                                    (spark/reduce-by-key min)))
+
+
+(comment highest-prices (->> outcode-prices
+                                     (spark/reduce-by-key max)))
+
+(comment outcode-prices (->> postcode-groups
+                                     (spark/map-to-pair (fn [t]
+                                                          (println (:price (._2 t)))
+                                                          (spark/tuple (._1 t) (:price (._2 t)))))))
+
+(comment count-rdd (->> (spark/map-to-pair (fn [w] (spark/tuple w 1)) words-rdd)
+                       (spark/reduce-by-key +) )
+
+         res (spark/map (s-de/key-value-fn (fn [k v] (str "(" k ", " v ")"))) output) )
